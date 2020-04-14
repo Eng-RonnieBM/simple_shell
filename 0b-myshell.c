77facf1c20227a0a45e82d6037271d2d2556d4dc
Eng-RonnieBM <ronniealberto@gmail.com>
2020-04-14 17:15:02 -0500

    main file with betty format

---
+++ 0b-myshell.c
@@ -0,0 +1,24 @@
+#include "library.h"
+
+/**
+ * main - main of the code for Simple Shell functions.
+ * @argc: argument's counter.
+ * @argv: argument's value.
+ * @envp: environment variables.
+ * Return: Always 0.
+ */
+int main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	int keep_prompt;
+	char **paths = fetch_paths(envp);
+
+	keep_prompt = 1;
+	while (keep_prompt)
+	{
+		handle_prompt(&keep_prompt, paths);
+	}
+	free(paths);
+	return (0);
+}
